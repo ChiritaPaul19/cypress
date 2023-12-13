8b07d16e2d59867f4c52cedf2c8d62adbcd2121b
ChiritaPaul19 <paul.chirita@endava.com>
2023-12-13 16:56:32 +0200

    Api chaining

---
+++ cypress_example/cypress/e2e/API/apiChanging.cy.js
@@ -0,0 +1,27 @@
+describe('API Chaining', () => {
+
+    it('Cookies dunctionality', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://jsonplaceholder.typicode.com/posts',
+        })
+            .then((response) => {
+                expect(response.status).to.eq(200)
+                const postId = response.body[0].id
+                return postId
+            })
+            .then((postId) => {
+                cy.request({
+                    method: 'GET',
+                    url: 'https://jsonplaceholder.typicode.com/comments',
+                    qs: {
+                        postid: postId
+                    }
+                })
+                    .then((response) => {
+                        expect(response.status).to.eq(200)
+                        expect(response.body).to.have.length.gt(5)
+                    })
+            })
+    })
+})
